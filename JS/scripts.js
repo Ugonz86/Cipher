5e63779e8faf05c52b5edeba81b5518b763144b4
Jordan Safford and Uriel Gonzalez <jordan@>
2019-10-21 16:36:11 -0700

    save alert result in new variable

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,5 @@
 var sentence = prompt("Enter a sentence:");
+alert (sentence);
 
-
-
-alert (sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(sentence.length-1).toUpperCase() + "!");
-
+var newSentence = alert(sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(sentence.length-1).toUpperCase() + " !");
 // alert (sentence.reverse());
