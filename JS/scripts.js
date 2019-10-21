39277f6398862572ea22e4f74f36c893b218080c
Jordan Safford and Uriel Gonzalez <uriel@>
2019-10-21 16:24:57 -0700

    Add initial files and code for the Cipher project.

---
+++ JS/scripts.js
@@ -0,0 +1,7 @@
+var sentence = prompt("Enter a sentence:");
+
+
+
+alert (sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(sentence.length-1).toUpperCase() + "!");
+
+// alert (sentence.reverse());
