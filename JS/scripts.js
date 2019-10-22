2ae4a9b93f49d4c6a5f4085051d57dd40061c605
Uriel Gonzalez and Wei Dai <wei@>
2019-10-22 16:19:48 -0700

    Add more js functions.

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,13 +6,22 @@
 //
 // var revSentence = alert(sentence.charAt(sentence.length-1).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(0).toUpperCase() + " !");
 
+
+
+// var sentence = prompt("Enter a sentence:");
+// alert (sentence);
+//
+// var result = alert(sentence.charAt(0).toUpperCase() + sentence.slice (-1).toUpperCase());
+//
+// var result = alert(sentence.slice (-1).toUpperCase() + sentence.charAt(0).toUpperCase());
+//
+// var result2 = alert(sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(sentence.length-1) + sentence.charAt(0).toUpperCase() + sentence.slice (-1).toUpperCase());
+//
+// var result2 = alert(sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(sentence.length-1) + sentence.slice (-1).toUpperCase() + sentence.charAt(0).toUpperCase());
+
+
+
 var sentence = prompt("Enter a sentence:");
 alert (sentence);
-
-var result = alert(sentence.charAt(0).toUpperCase() + sentence.slice (-1).toUpperCase());
-
-var result = alert(sentence.slice (-1).toUpperCase() + sentence.charAt(0).toUpperCase());
-
-var result2 = alert(sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(sentence.length-1) + sentence.charAt(0).toUpperCase() + sentence.slice (-1).toUpperCase());
-
-var result2 = alert(sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(sentence.length-1) + sentence.slice (-1).toUpperCase() + sentence.charAt(0).toUpperCase());
+var wordCounter = ("Your sentence has " + sentence.length + " letters!");
+alert (wordCounter);
