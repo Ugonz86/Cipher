b8c282f02b960eb880d0cabf0acfc074d0f5e90b
Ugonz86 <ugonzalez86@gmail.com>
2019-10-22 08:16:10 -0700

    add more functions

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,3 +3,6 @@ alert (sentence);
 
 var newSentence = alert(sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(sentence.length-1).toUpperCase() + " !");
 // alert (sentence.reverse());
+
+var revSentence = alert(sentence.charAt(sentence.length-1).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(0).toUpperCase() + " !");
+// var revSentence = alert(sentence.split("").reverse().join("").split(" ").reverse().join(" "));
