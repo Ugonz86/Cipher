5a8858dabaa95e382b6d392c65c7b176d7a00e3e
Ugonz86 <ugonzalez86@gmail.com>
2019-10-22 21:58:01 -0700

    Add more jquery functionality.

--- JS/scripts.js
+++ JS/scripts.js
@@ -19,9 +19,25 @@
 //
 // var result2 = alert(sentence.charAt(0).toUpperCase() + sentence.slice(1,sentence.length-1) + sentence.charAt(sentence.length-1) + sentence.slice (-1).toUpperCase() + sentence.charAt(0).toUpperCase());
 
-
-
 var sentence = prompt("Enter a sentence:");
 alert (sentence);
-var wordCounter = ("Your sentence has " + sentence.length + " letters!");
+
+var wordCounter = ("Your sentence has " + sentence.length + " characters!");
 alert (wordCounter);
+
+// var sentence = (prompt("Enter a sentence:"));
+var result = letterCounter(sentence) / 2;
+alert ("Your sentence letter count divided by 2 equals " + result + "!");
+function letterCounter(userInput) {
+
+    var letters = 0;
+    var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var letras = alphabet.split("");
+    for (var i=0; i<userInput.length;i++) {
+        if (letras.indexOf(userInput[i]) > -1) {
+            letters = letters + 1;
+        }
+    }
+    alert ("Your sentence has " + letters + " letters!");
+    return letters;
+}
